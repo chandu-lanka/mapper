b2f0fb2c23ed1e67193a2694bb90779dd091c3b5
TriangleDevv <chandu.lanka13@gmail.com>
2021-09-26 19:08:28 +0530

    got tiles(rects) getting placed

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from data.globals import *
 win = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(f"Mapper ({WINDOW_SIZE[0]}x{WINDOW_SIZE[1]})")
 
+## variables ##
+tile_map = []
+
 running = True
 while running:
     win.fill((0,0,0))
@@ -16,7 +19,10 @@ while running:
     win.blit(s, (cursor[0], cursor[1]))
 
     if pygame.mouse.get_pressed()[0]:
-        pygame.draw.rect(win, (255,255,0), (cursor[0], cursor[1], 50, 50))
+        tile_map.append((cursor[0], cursor[1]))
+
+    for tile in tile_map:
+        pygame.draw.rect(win, (255,255,255), (tile[0], tile[1], 50, 50))
 
     for ev in pygame.event.get():
         if ev.type == QUIT:
