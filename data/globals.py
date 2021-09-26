b4e6e8723f0f2cc3e564380c9c859a011c61236b
TriangleDevv <chandu.lanka13@gmail.com>
2021-09-26 09:12:03 +0530

    i guess it kind of works, well it just doesnt display on the screen for long so i have to work on that

--- data/globals.py
+++ data/globals.py
@@ -1,1 +1,6 @@
+import pygame
+from pygame.locals import *
+
 WINDOW_SIZE = (600, 400)
+FPS = 60
+clock = pygame.time.Clock()
--- main.py
+++ main.py
@@ -8,8 +8,21 @@ pygame.display.set_caption(f"Mapper ({WINDOW_SIZE[0]}x{WINDOW_SIZE[1]})")
 
 running = True
 while running:
+    win.fill((0,0,0))
+    cursor = pygame.mouse.get_pos()
+    s = pygame.Surface((50,50))
+    s.set_alpha(75)
+    s.fill((255,255,0))
+    win.blit(s, (cursor[0], cursor[1]))
+
+    if pygame.mouse.get_pressed()[0]:
+        pygame.draw.rect(win, (255,255,0), (cursor[0], cursor[1], 50, 50))
+
     for ev in pygame.event.get():
         if ev.type == QUIT:
             running = False;
             pygame.quit()
             sys.exit()
+
+    pygame.display.update()
+    clock.tick(FPS)
